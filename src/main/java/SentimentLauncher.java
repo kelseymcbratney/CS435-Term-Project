4b53c7a9d27489efc11fffcb0bcf4afffddd5aae
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-11-29 01:05:28 -0700

    test

--- src/main/java/SentimentLauncher.java
+++ src/main/java/SentimentLauncher.java
@@ -36,37 +36,37 @@ public class SentimentLauncher {
 
     job.waitForCompletion(true);
 
-    // Set up the second map-reduce job to calculate inverse document frequency
-    // (IDF)
-    job = Job.getInstance(conf, "TF-IDF Job - IDF");
-
-    job.setJarByClass(TFIDFJob.class);
-    job.setMapperClass(TFIDFJob.IDFMapper.class);
-    job.setMapOutputKeyClass(Text.class);
-    job.setMapOutputValueClass(Text.class);
-    job.setReducerClass(TFIDFJob.SumReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(Text.class);
-
-    FileInputFormat.addInputPath(job, new Path(args[1] + "/temp"));
-    FileOutputFormat.setOutputPath(job, new Path(args[1] + "/idf"));
-
-    job.waitForCompletion(true);
-
-    // Set up the third map-reduce job to calculate TF-IDF
-    job = Job.getInstance(conf, "TF-IDF Job - TF-IDF");
-
-    job.setJarByClass(TFIDFJob.class);
-    job.setMapperClass(TFIDFJob.TFMapper.class);
-    job.setMapOutputKeyClass(Text.class);
-    job.setMapOutputValueClass(Text.class);
-    job.setReducerClass(TFIDFJob.TFIDFReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(Text.class);
-
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1] + "/tfidf"));
-
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    // // Set up the second map-reduce job to calculate inverse document frequency
+    // // (IDF)
+    // job = Job.getInstance(conf, "TF-IDF Job - IDF");
+    //
+    // job.setJarByClass(TFIDFJob.class);
+    // job.setMapperClass(TFIDFJob.IDFMapper.class);
+    // job.setMapOutputKeyClass(Text.class);
+    // job.setMapOutputValueClass(Text.class);
+    // job.setReducerClass(TFIDFJob.SumReducer.class);
+    // job.setOutputKeyClass(Text.class);
+    // job.setOutputValueClass(Text.class);
+    //
+    // FileInputFormat.addInputPath(job, new Path(args[1] + "/temp"));
+    // FileOutputFormat.setOutputPath(job, new Path(args[1] + "/idf"));
+    //
+    // job.waitForCompletion(true);
+    //
+    // // Set up the third map-reduce job to calculate TF-IDF
+    // job = Job.getInstance(conf, "TF-IDF Job - TF-IDF");
+    //
+    // job.setJarByClass(TFIDFJob.class);
+    // job.setMapperClass(TFIDFJob.TFMapper.class);
+    // job.setMapOutputKeyClass(Text.class);
+    // job.setMapOutputValueClass(Text.class);
+    // job.setReducerClass(TFIDFJob.TFIDFReducer.class);
+    // job.setOutputKeyClass(Text.class);
+    // job.setOutputValueClass(Text.class);
+    //
+    // FileInputFormat.addInputPath(job, new Path(args[0]));
+    // FileOutputFormat.setOutputPath(job, new Path(args[1] + "/tfidf"));
+    //
+    // System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
