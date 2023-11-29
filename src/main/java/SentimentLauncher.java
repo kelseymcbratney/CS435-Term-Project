bc1973e23f138c4f4eebd25a82b05dfa0ab895eb
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-11-29 16:52:14 -0700

    test

--- src/main/java/SentimentLauncher.java
+++ src/main/java/SentimentLauncher.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextOutputFormat;
 
 public class SentimentLauncher {
 
