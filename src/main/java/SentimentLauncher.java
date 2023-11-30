7e9c0907d4728e8836f6ac284e30f98001e24a94
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-11-30 14:19:10 -0700

    uri import

--- src/main/java/SentimentLauncher.java
+++ src/main/java/SentimentLauncher.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.Text;
+import java.net.URI;
 
 public class SentimentLauncher {
   public static void main(String[] args) throws Exception {
