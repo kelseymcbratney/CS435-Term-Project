f5e43b83aa658cbdec1c858d24e665a435a0994b
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-11-30 14:16:21 -0700

    fix imports

--- src/main/java/SentimentLauncher.java
+++ src/main/java/SentimentLauncher.java
@@ -1,14 +1,12 @@
 package SentimentLauncher;
 
-import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.Counter;
 
 public class SentimentLauncher {
   public static void main(String[] args) throws Exception {
