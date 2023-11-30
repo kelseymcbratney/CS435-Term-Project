84007f98201ce5fdd890caea35a7e6ff12ee16e0
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-11-29 17:16:09 -0700

    sda

--- src/main/java/TFIDFJob.java
+++ src/main/java/TFIDFJob.java
@@ -89,7 +89,7 @@ public class TFIDFJob {
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
       // Continue with your processing
 
-      context.write(new Text(key), new Text(value));
+      context.write(new Text(String.valueOf(key.get())), new Text(value));
     }
   }
 
