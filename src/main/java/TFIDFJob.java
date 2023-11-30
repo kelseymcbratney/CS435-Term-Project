24a1a8ba869a38d05fabfdb02f47bbb79e8f56d0
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-11-30 03:46:29 -0700

    add imports

--- src/main/java/TFIDFJob.java
+++ src/main/java/TFIDFJob.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.commons.lang3.tuple.Triple;
