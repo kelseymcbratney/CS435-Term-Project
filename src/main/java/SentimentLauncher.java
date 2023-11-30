0a6cafa63111c0b002a18796eebd94d389a17163
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-11-29 19:54:50 -0700

    fix imports

--- src/main/java/SentimentLauncher.java
+++ src/main/java/SentimentLauncher.java
@@ -3,6 +3,9 @@ package SentimentLauncher;
 import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 
