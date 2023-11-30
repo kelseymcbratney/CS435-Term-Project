155242380f01382106d8bf122485e418a18bb2c7
Kelsey McBratney <kelseymcbratney@gmail.com>
2023-11-30 02:00:22 -0700

    do nothing

--- src/main/java/TFIDFJob.java
+++ src/main/java/TFIDFJob.java
@@ -31,7 +31,6 @@ public class TFIDFJob {
   }
 
   public static class TFTokenizer extends Mapper<LongWritable, Text, Text, Text> {
-    private final Text docID = new Text();
     private final Text RatingUnigramCount = new Text();
     private final ObjectMapper mapper = new ObjectMapper();
     private static Set<String> stopWords;
@@ -159,56 +158,11 @@ public class TFIDFJob {
     }
   }
 
-  public static class SumReducer extends Reducer<Text, Text, Text, Text> {
-    private final Text result = new Text();
-    private String rating;
-
+public static class SumReducer extends Reducer<Text, Text, Text, Text> {
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-      System.err.println("Reducer key: " + key.toString());
-      // Initialize a map to store the count of each unigram
-      Map<String, Integer> unigramCountMap = new HashMap<>();
-
-      // Iterate through the values and count the occurrences of each unigram
-      int totalWords = 0; // Total words in the document for calculating TF
-
-      for (Text value : values) {
-        String[] parts = value.toString().split("\t");
-
-        // Check if the value has the expected format
-        if (parts.length >= 3) {
-          String unigram = parts[1]; // Assuming the unigram is at index 2
-          int count = Integer.parseInt(parts[2]); // Assuming the count is at index 1
-          rating = parts[0];
-
-          // Update the count in the map
-          unigramCountMap.put(unigram, unigramCountMap.getOrDefault(unigram, 0) + count);
-
-          // Accumulate total words
-          totalWords += count;
-        } else {
-          // Log a warning or handle the unexpected format
-          System.err.println("Unexpected format: " + value.toString());
-        }
-      }
-
-      // Build the result string with TF values and the overall rating
-      StringBuilder resultBuilder = new StringBuilder();
-      for (Map.Entry<String, Integer> entry : unigramCountMap.entrySet()) {
-        String unigram = entry.getKey();
-        int count = entry.getValue();
-
-        // Calculate TF (Term Frequency)
-        double tf = (double) count / totalWords;
-
-        resultBuilder.append(rating).append("\t").append(unigram).append("\t").append(tf).append("\n");
-      }
-
-      // Set the result text
-      result.set(resultBuilder.toString().trim()); // Trim to remove trailing newline
-
-      // Emit the result for the key (overall rating)
-      context.write(key, result);
+        // Do nothing
     }
+}
 
     // public static class IDFMapper extends Mapper<Object, Text, Text, Text> {
     // private final Text unigram = new Text();
